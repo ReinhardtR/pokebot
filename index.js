c21bc845ed7a049bc162fbca2e80b24351c50a52
Livi // Frederik <lividk123456789@gmail.com>
2021-03-15 13:30:32 +0100

    Embed Implementation
    - Added MENU embed
    - Added color scheme
    - Added number-reactions
    - Added global prefix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const PREFIX = "p!";
+const menuGUI = "https://i.imgur.com/Sx2mQTH.png";
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -11,6 +13,26 @@ client.on("message", (msg) => {
 
   if (msg.content === "yo") {
     msg.channel.send("yo");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content == PREFIX + "menu") {
+    const embed = {
+      title: "PokéBot Menu",
+      color: 53380,
+      image: {
+        url: menuGUI,
+      },
+    };
+
+    message.channel.send({ embed: embed }).then((embedMessage) => {
+      const emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"];
+      for (let i = 0; i < emojis.length; i++) {
+        if (i == 5) return;
+        embedMessage.react(emojis[i]);
+      }
+    });
   }
 });
 
