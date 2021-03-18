cf023912de7abcf27f565b4a0d461513261fc7bd
Reinhardt Rijna <reinhardtrijna@gmail.com>
2021-03-18 13:26:40 +0100

    convert pokedex into a function

--- commands/profile/pokedex.js
+++ commands/profile/pokedex.js
@@ -2,11 +2,13 @@ module.exports = {
   name: "pokedex",
   description: "Get data from a user, and visualize their pokedex.",
   execute(msg, args) {
-    sendPokedex(msg);
+    getPokedex(msg).then((embed) => {
+      msg.channel.send(embed);
+    });
   },
 };
 
-function sendPokedex(msg) {
+function getPokedex(msg) {
   const Discord = require("discord.js");
   const Canvas = require("canvas");
   const Pokedex = require("pokedex-promise-v2");
@@ -35,106 +37,107 @@ function sendPokedex(msg) {
   ctx.fillStyle = "rgb(20,20,20)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   // Draw pokemon sprites
-  getData.then(async (data) => {
-    // Size and position variables
-    const spriteSize = 192;
-    const gap = 16;
-    var x = 10;
-    var y = 64;
+  const embed = new Promise((resolve, reject) => {
+    getData.then(async (data) => {
+      // Size and position variables
+      const spriteSize = 192;
+      const gap = 16;
+      var x = 10;
+      var y = 64;
 
-    // Sort by pokemon id
-    const pokemons = await data.sort((a, b) => a.id - b.id);
+      // Sort by pokemon id
+      const pokemons = await data.sort((a, b) => a.id - b.id);
 
-    // Test array to check if owned pokemons are colored
-    const array = [8, 23, 48, 57, 58, 110, 121, 122, 139, 140, 149];
+      // Test array to check if owned pokemons are colored
+      const array = [8, 23, 48, 57, 58, 110, 121, 122, 139, 140, 149];
 
-    // Preload images
-    Promise.all(
-      pokemons.map((pokemon) => {
-        return new Promise((resolve, reject) => {
-          const img = new Canvas.Image();
-          img.src = pokemon.sprite;
-          img.onload = resolve;
-          pokemon.sprite = img;
-        });
-      })
-    )
-      .then(() => {
-        // Draw pokemon sprites and names
-        pokemons.map((pokemon, index) => {
-          // Adjust position
-          if (x + spriteSize + gap > canvas.width) {
-            y += spriteSize + gap * 3.3;
-            x = 10;
-          } else if (index !== 0) {
-            x += spriteSize + gap;
-          }
+      // Preload images
+      Promise.all(
+        pokemons.map((pokemon) => {
+          return new Promise((resolve, reject) => {
+            const img = new Canvas.Image();
+            img.src = pokemon.sprite;
+            img.onload = resolve;
+            pokemon.sprite = img;
+          });
+        })
+      )
+        .then(() => {
+          // Draw pokemon sprites and names
+          pokemons.map((pokemon, index) => {
+            // Adjust position
+            if (x + spriteSize + gap > canvas.width) {
+              y += spriteSize + gap * 3.3;
+              x = 10;
+            } else if (index !== 0) {
+              x += spriteSize + gap;
+            }
 
-          // Do you have that pokemon
-          const isCaught = array.includes(pokemon.id);
+            // Do you have that pokemon
+            const isCaught = array.includes(pokemon.id);
 
-          // Capitalize pokemon name
-          const name = isCaught
-            ? pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-            : "???";
+            // Capitalize pokemon name
+            const name = isCaught
+              ? pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+              : "???";
 
-          // Text settings
-          ctx.font = "32px sans-serif";
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          // Calculate text position, needed because of textAlign
-          const textPosX = x + spriteSize / 2;
-          // Draw text and image
-          ctx.fillText(name, textPosX, y);
-          ctx.drawImage(pokemon.sprite, x, y, spriteSize, spriteSize);
-          // Recolor unowned pokemons
-          if (!isCaught) {
-            // Recolor each pixel
-            const pixels = ctx.getImageData(x, y, spriteSize, spriteSize);
-            for (var i = 0; i < pixels.data.length; i += 4) {
-              for (var j = 0; j < 3; j++) {
-                if (pixels.data[i + j] !== 20) {
-                  pixels.data[i + j] = 5;
+            // Text settings
+            ctx.font = "32px sans-serif";
+            ctx.fillStyle = "#ffffff";
+            ctx.textAlign = "center";
+            // Calculate text position, needed because of textAlign
+            const textPosX = x + spriteSize / 2;
+            // Draw text and image
+            ctx.fillText(name, textPosX, y);
+            ctx.drawImage(pokemon.sprite, x, y, spriteSize, spriteSize);
+            // Recolor unowned pokemons
+            if (!isCaught) {
+              // Recolor each pixel
+              const pixels = ctx.getImageData(x, y, spriteSize, spriteSize);
+              for (var i = 0; i < pixels.data.length; i += 4) {
+                for (var j = 0; j < 3; j++) {
+                  if (pixels.data[i + j] !== 20) {
+                    pixels.data[i + j] = 5;
+                  }
                 }
               }
+              ctx.putImageData(pixels, x, y);
             }
-            ctx.putImageData(pixels, x, y);
-          }
+          });
+        })
+        .then(() => {
+          // Text settings
+          ctx.font = "100px sans-serif";
+          ctx.fillStyle = "#ffffff";
+          ctx.textAlign = "end";
+          ctx.textBaseline = "bottom";
+          // User tag
+          ctx.fillText(msg.author.tag, canvas.width - gap, canvas.height - gap);
+          // Pokemon count
+          ctx.fillText(
+            `${array.length}/151`,
+            canvas.width - gap,
+            canvas.height - (100 + gap)
+          );
+
+          // Create image file
+          console.log("sending");
+          const attachment = new Discord.MessageAttachment(
+            canvas.toBuffer(),
+            "pokedex.png"
+          );
+
+          // Create embed with image attached
+          const pokedexEmbed = new Discord.MessageEmbed()
+            .setTitle("Pokédex")
+            .setDescription(msg.author)
+            .setColor(53380)
+            .attachFiles(attachment)
+            .setImage(`attachment://${attachment.name}`)
+            .setThumbnail(msg.author.avatarURL());
+          resolve(pokedexEmbed);
         });
-      })
-      .then(() => {
-        // Text settings
-        ctx.font = "100px sans-serif";
-        ctx.fillStyle = "#ffffff";
-        ctx.textAlign = "end";
-        ctx.textBaseline = "bottom";
-        // User tag
-        ctx.fillText(msg.author.tag, canvas.width - gap, canvas.height - gap);
-        // Pokemon count
-        ctx.fillText(
-          `${array.length}/151`,
-          canvas.width - gap,
-          canvas.height - (100 + gap)
-        );
-
-        // Create image file
-        console.log("sending");
-        const attachment = new Discord.MessageAttachment(
-          canvas.toBuffer(),
-          "pokedex.png"
-        );
-
-        // Create embed with image attached
-        const embed = new Discord.MessageEmbed()
-          .setTitle("Pokédex")
-          .setDescription(msg.author)
-          .setColor(53380)
-          .attachFiles(attachment)
-          .setImage(`attachment://${attachment.name}`)
-          .setThumbnail(msg.author.avatarURL());
-
-        // Send embed
-        msg.channel.send({ embed });
-      });
+    });
   });
+  return { embed };
 }
