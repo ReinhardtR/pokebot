c414deedb29abdefd1d5c646359c81ca68bd1e33
Livi // Frederik <lividk123456789@gmail.com>
2021-03-18 11:21:28 +0100

    Await Reactions Filter
    - Added reaction filter
    - Added Player Profile Menu

--- commands/menu/menu.js
+++ commands/menu/menu.js
@@ -6,6 +6,15 @@ module.exports = {
   },
 };
 
+// Awaiting reactions
+const filter = (reaction, user) => {
+  return (
+    ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"].includes(
+      reaction.emoji.name
+    ) && user.id === msg.author.id
+  );
+};
+
 function openMenuGUI(msg) {
   const menuGUI = "https://i.imgur.com/Sx2mQTH.png";
   const embed = {
@@ -22,5 +31,28 @@ function openMenuGUI(msg) {
       if (i === 5) return;
       embedMsg.react(emojis[i]);
     }
+    embedMsg
+      .awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] })
+      .then((collected) => {
+        const reaction = collected.first();
+        console.log(reaction.emoji.name);
+
+        if (reaction.emoji.name === "1️⃣") {
+          msg.reply("pogU");
+        } else {
+          msg.reply("sadge");
+        }
+      });
   });
+}
+
+function menuPlayerProfile(msg) {
+  const commandEmbed = {
+    plainText: "test",
+    description: "A complete list of all player profile commands!",
+    author: {
+      name: "Player Profile Commands",
+    },
+    color: 53380,
+  };
 }
