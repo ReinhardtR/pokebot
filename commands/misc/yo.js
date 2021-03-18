fbc8ede1a87cea9afa157f73c9ee7edcd6636327
Reinhardt Rijna <reinhardtrijna@gmail.com>
2021-03-18 11:21:14 +0100

    args requirement

--- commands/misc/yo.js
+++ commands/misc/yo.js
@@ -1,7 +1,15 @@
 module.exports = {
   name: "yo",
   description: "Test - replies with yo.",
+  args: true,
+  usage: "<boi>",
   execute(msg, args) {
-    msg.channel.send("yo");
+    let reply = "yo ";
+
+    if (args[0] === "boi") {
+      reply += "boi";
+    }
+
+    msg.channel.send(reply);
   },
 };
