e8f94517eed70ac087281d5d800a4640911e7999
Reinhardt Rijna <reinhardtrijna@gmail.com>
2021-03-18 12:01:22 +0100

    reaction callback

--- commands/menu/menu.js
+++ commands/menu/menu.js
@@ -6,15 +6,6 @@ module.exports = {
   },
 };
 
-// Awaiting reactions
-const filter = (reaction, user) => {
-  return (
-    ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"].includes(
-      reaction.emoji.name
-    ) && user.id === msg.author.id
-  );
-};
-
 function openMenuGUI(msg) {
   const menuGUI = "https://i.imgur.com/Sx2mQTH.png";
   const embed = {
@@ -27,22 +18,40 @@ function openMenuGUI(msg) {
 
   msg.channel.send({ embed }).then((embedMsg) => {
     const emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"];
-    for (let i = 0; i < emojis.length; i++) {
-      if (i === 5) return;
-      embedMsg.react(emojis[i]);
-    }
-    embedMsg
-      .awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] })
-      .then((collected) => {
-        const reaction = collected.first();
-        console.log(reaction.emoji.name);
+    const reacts = emojis.map((emoji, index) => {
+      if (index >= 5) return;
+      embedMsg.react(emoji);
+    });
+    Promise.all(reacts).then(() => {
+      // Awaiting reactions
+      const filter = (reaction, user) => {
+        const included = [
+          "1️⃣",
+          "2️⃣",
+          "3️⃣",
+          "4️⃣",
+          "5️⃣",
+          "6️⃣",
+          "7️⃣",
+          "8️⃣",
+          "9️⃣",
+        ].includes(reaction.emoji.name);
+        const id = msg.author.id === user.id;
+        return included && id;
+      };
 
-        if (reaction.emoji.name === "1️⃣") {
-          msg.reply("pogU");
-        } else {
-          msg.reply("sadge");
-        }
-      });
+      embedMsg
+        .awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] })
+        .then((collected) => {
+          const reaction = collected.first();
+
+          if (reaction.emoji.name === "1️⃣") {
+            msg.reply("pogU");
+          } else {
+            msg.reply("sadge");
+          }
+        });
+    });
   });
 }
 
