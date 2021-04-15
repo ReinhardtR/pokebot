8a8529f7c1d6ae7e4bf51e7cf346d07ac94df2e8
Reinhardt Rijna <reinhardtrijna@gmail.com>
2021-04-15 11:04:47 +0200

    added types to pokemon command

--- commands/misc/pokemon.js
+++ commands/misc/pokemon.js
@@ -12,9 +12,11 @@ module.exports = {
     const pokemonArg = args[0].toLowerCase();
 
     // Check if the arg, results in a valid Pokémon.
+    console.log(pokemonArg);
     const pokemon = pokemons.find(
-      (pkmn) => pkmn.name === pokemonArg || pkmn.id === pokemonArg
+      (pkmn) => pkmn.name === pokemonArg || pkmn.id == pokemonArg
     );
+    console.log(pokemon);
 
     // If there is no Pokémon found, from the provided arg, reply with a feedback message.
     if (!pokemon) {
@@ -31,22 +33,54 @@ module.exports = {
     const hasCaughtPokemon = userPokedex.includes(pokemon.id);
 
     // Get image of Pokémon types.
-    const pokemonTypes = getImageOfPokemonTypes(pokemon.types);
+    const pokemonTypesImage = await getImageOfPokemonTypes(pokemon.types);
+    const pokemonTypesAttachment = new Discord.MessageAttachment(
+      pokemonTypesImage,
+      "pokemonTypes.png"
+    );
 
     // Create an embed representing the Pokémon.
     const embed = new Discord.MessageEmbed()
       .setTitle(upperCaseString(pokemon.name))
       .setThumbnail(pokemon.sprites.front)
-      .addFields({ name: "Rarity", value: pokemon.rarity })
+      .addFields(
+        { name: "Rarity", value: pokemon.rarity, inline: true },
+        { name: "Caught", value: hasCaughtPokemon ? "✅" : "❌", inline: true }
+      )
+      .attachFiles(pokemonTypesAttachment)
+      .setImage(`attachment://${pokemonTypesAttachment.name}`)
       .setFooter(`ID: ${pokemon.id}`);
     msg.channel.send({ embed });
   },
 };
 
-function getImageOfPokemonTypes(types) {
+const getImageOfPokemonTypes = async (types) => {
+  // Dependencies.
   const Canvas = require("canvas");
 
-  const canvasWidth = 90 * types.length;
+  // Size and position variables;
+  const typeWidth = 45;
+  const typeHeight = 20;
+  const typeGap = 5;
 
-  const canvas = Canvas.createCanvas(canvasWidth, 40);
-}
+  // Calculate canvas width, depending on the amount of types.
+  const canvasWidth = (typeWidth + typeGap) * types.length;
+  const canvas = Canvas.createCanvas(canvasWidth, typeHeight);
+  const ctx = canvas.getContext("2d");
+
+  const imagesOfTypes = await Promise.all(
+    types.map(async (type) => {
+      const url = `https://raw.githubusercontent.com/ReinhardtR/pokebot/main/images/types/${type}.png`;
+      const image = await Canvas.loadImage(url);
+      return image;
+    })
+  );
+
+  imagesOfTypes.forEach((typeImage, index) => {
+    const posX = (typeWidth + typeGap) * index;
+    const posY = 0;
+    ctx.drawImage(typeImage, posX, posY, typeWidth, typeHeight);
+  });
+
+  return canvas.toBuffer();
+};
